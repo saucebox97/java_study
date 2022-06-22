edf26c9347dd3844efd73675143d311b32b2ce68
saucebox97 <tjdqls970208@naver.com>
2022-06-22 18:19:49 +0900

    5월 18일 자바 Day1

--- src/datastructure/chap09/그리디03.java
+++ src/datastructure/chap09/그리디03.java
@@ -1,40 +1,27 @@
 package datastructure.chap09;
 
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 // 백준 1715
 public class 그리디03 {
 
     public static void main(String[] args) {
 
-        // 우선순위 큐 - 자동정렬기능
-        PriorityQueue<Integer> cardList = new PriorityQueue<>();
-        cardList.offer(10);
-        cardList.offer(20);
-        cardList.offer(40);
-
-        System.out.println(cardList);
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt(); // 카드 묶음 수
+        PriorityQueue<Integer> cardList = new PriorityQueue<>();//우선순위큐는 자기가 들어갈곳을 비교해서 찾아들어감
+        for (int i = 0; i < N; i++) {
+            cardList.add(sc.nextInt());
+        }
 
         int total = 0;
-
-        Integer n1 = cardList.poll();
-        Integer n2 = cardList.poll();
-        int e = n1 + n2;
-        total += e;
-        cardList.offer(e);
-        System.out.println(cardList);
-
-        n1 = cardList.poll();
-        n2 = cardList.poll();
-        e = n1 + n2;
-        total += e;
-        cardList.offer(e);
-        System.out.println(cardList);
-
-        System.out.println("total = " + total);
+        while (cardList.size() != 1) { // 숫자 1개남을때까지
+            int data1 = cardList.poll(); // poll 앞에꺼 제거하면서 값리턴
+            int data2 = cardList.poll();
+            total += data1 + data2;
+            cardList.offer(data1 + data2); // offer 추가하면서 값 리턴 add해도됌
+        }
+        System.out.println(total);
 
     }
 }
