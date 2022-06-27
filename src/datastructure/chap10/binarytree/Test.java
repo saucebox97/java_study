c500791ae017b0032fb3533e3fba40dfdacf0de8
saucebox97 <tjdqls970208@naver.com>
2022-06-27 18:18:01 +0900

    5월 18일 자바 Day1

--- src/datastructure/chap10/binarytree/Test.java
+++ src/datastructure/chap10/binarytree/Test.java
@@ -1,5 +1,8 @@
 package datastructure.chap10.binarytree;
 
+import java.util.TreeMap;
+import java.util.TreeSet;
+
 public class Test {
 
     public static void main(String[] args) {
@@ -44,6 +47,46 @@ public class Test {
         tree.add(100);
         System.out.println(tree.findMin());
         System.out.println(tree.findMax());
+
+        System.out.println("==============트리 출력===============");
+
+        tree.delete(36);
+
+        tree.display();
+
+        // 트리의 문제점
+        BinaryTree tree2 = new BinaryTree();
+
+        tree2.add(50);
+        tree2.add(40);
+        tree2.add(30);
+        tree2.add(20);
+
+        System.out.println("============ 이진트리의 문제점 =============");
+        tree2.display();
+        // 자식 2명만 돼고 잘못하면 선형데이터(자료를 구성하는 데이터를 순차적으로 나열시킨 형태)됌
+
+        System.out.println("===========API 트리사용=============");
+
+        // TreeSet : Set형태로 Tree를 구현
+        TreeSet<Object> treeSet = new TreeSet<>();
+
+        treeSet.add(50);
+        treeSet.add(77);
+        treeSet.add(27);
+        treeSet.add(7);
+        treeSet.add(67);
+
+        System.out.println(treeSet);
+
+        // TreeMap : Map형태로 트리를 구성
+        TreeMap<String, Integer> TreeMap = new TreeMap<>();
+        TreeMap.put("멍멍이", 44);
+        TreeMap.put("야옹이", 30);
+        TreeMap.put("짹짹이", 55);
+        TreeMap.put("강쥐", 155);
+
+        System.out.println(TreeMap);
 
     }
 }
